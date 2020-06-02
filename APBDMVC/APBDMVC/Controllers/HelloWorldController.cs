b77d0f99c52cdb01e0d1e4284609e4f144d4cb2b
Maciej Malewicz <maciekmalewicz2012b@gmail.com>
2020-06-02 17:14:42 +0200

    finishedTutorial

---
+++ APBDMVC/APBDMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBDMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int numTimes = 3)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
